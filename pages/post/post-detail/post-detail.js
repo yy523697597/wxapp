97e7c62e8075bb85e67342ada3a0d9fe0a3cafaf
yu yi <523697597@qq.com>
2017-05-28 18:14:42 +0800

    V2.2 完成分享按钮功能

--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -58,6 +58,23 @@ Page({
       duration: 1000
     })
   },
+  onShareTap: function (evt) {
+    console.log(1)
+    var itemList = [
+      "分享到朋友圈",
+      "分享给微信好友"
+    ];
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor:"#355A83",
+      success: function (res) {
+        wx.showModal({
+          title: itemList[res.tapIndex],
+          content:"是否取消? "+res.cancel+" 现在无法分享"
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */

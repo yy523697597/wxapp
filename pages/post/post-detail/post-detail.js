5bf1e6f9da3cf45e10e75870b983b0f904ea8454
yy523697597 <523697597@qq.com>
2017-05-28 14:39:39 +0800

    V2.0 文章详情页收藏状态切换，通过缓存来保存页面收藏状态

--- pages/post/post-detail/post-detail.js
+++ pages/post/post-detail/post-detail.js
@@ -6,18 +6,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    currentPostId: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 获取文章详情内容
     var postid = options.id;
     var postDetail = postsData.postList[postid];
     this.setData(postDetail);
+    // 设置文章id为页面共享
+    this.data.currentPostId = postid;
+    // 获取缓存
+    var collectionList = wx.getStorageSync('collectionList');
+    if (collectionList) {
+      // 获取文章收藏状态
+      var collectionStatus = collectionList[postid];
+      // 更新文章收藏状态
+      this.setData({
+        collectionStatus: collectionStatus
+      });
+    } else {
+      // 如果不存在缓存就设置一个新缓存
+      var collectionList = {};
+      wx.setStorageSync('collectionList', collectionList);
+    }
   },
-
+  onCollectionTap: function (event) {
+    var collectionList = wx.getStorageSync('collectionList');
+    // 获取文章缓存状态
+    var collectionStatus = collectionList[this.data.currentPostId];
+    // 文章收藏状态切换
+    collectionStatus = !collectionStatus;
+    // 存储文章收藏状态
+    collectionList[this.data.currentPostId] = collectionStatus;
+    wx.setStorageSync('collectionList', collectionList);
+    // 更新文章收藏状态
+    this.setData({
+      collectionStatus: collectionStatus
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */

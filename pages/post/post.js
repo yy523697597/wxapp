983c2a909a13cd31272fa3d3bb1231da6b0a8cf5
yu yi <523697597@qq.com>
2017-06-01 21:42:57 +0800

    V3.1 修正收藏和阅读数的显示位置，为添加点击文章增加阅读数功能做准备

--- pages/post/post.js
+++ pages/post/post.js
@@ -6,27 +6,50 @@ Page({
    * 页面的初始数据
    */
   data: {
+
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // var postid = options.id;
+
+
     this.setData({
       swiperInfo: postsData.swiperInfo,
       postList: postsData.postList
     });
+
+    // var readingList = wx.getStorageSync('readingList');
+    // if (readingList) {
+    //   // 获取文章收藏状态
+    //   var readingNum = readingList[postid];
+    //   // 更新文章收藏状态
+    //   this.setData({
+    //     readingNum: readingNum
+    //   });
+    // } else {
+    //   // 如果不存在缓存就设置一个新缓存
+    //   var readingList = {};
+    //   wx.setStorageSync('readingList', readingList);
+    // }
   },
   onPostTap: function (evt) {
     var postid = evt.currentTarget.dataset.postid;
-    // console.log('tapid is '+postid);
+    // var readingList = wx.getStorageSync('readingList');
     wx.navigateTo({
       url: 'post-detail/post-detail?id=' + postid
-    })
+    });
+
+    // readingList[postid] = postsData.postList[postid].reading;
+    // wx.setStorageSync('readingList', readingList);
   },
-  onSwiperTap:function(evt){
+  onSwiperTap: function (evt) {
     var postid = evt.currentTarget.dataset.postid;
     wx.navigateTo({
       url: 'post-detail/post-detail?id=' + postid
-    })
+    });
+
+    // postsData.postList[postid].reading++;
   }
 })

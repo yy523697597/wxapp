4adf08996419e832d42a2b37ca64da92af66f61d
yu yi <523697597@qq.com>
2017-06-04 20:57:16 +0800

    V4.8  优化向豆瓣请求数据的频率

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -8,7 +8,11 @@ Page({
    */
   data: {
     navigationTitle: '',
-    movies: {}
+    movies: {},
+    requestUrl: '',
+    totalCount: 18,
+    isEmpty: true,
+    isFreshing: false
   },
 
   /**
@@ -23,18 +27,23 @@ Page({
     var dataUrl = '';
     switch (navigationTitle) {
       case "正在上映的电影-成都":
-        dataUrl = baseUrl + '/v2/movie/in_theaters?count=18';
+        dataUrl = baseUrl + '/v2/movie/in_theaters';
         break;
       case "即将上映的电影":
-        dataUrl = baseUrl + '/v2/movie/coming_soon?count=18';
+        dataUrl = baseUrl + '/v2/movie/coming_soon';
         break;
       case "豆瓣电影Top250":
-        dataUrl = baseUrl + '/v2/movie/top250?count=18';
+        dataUrl = baseUrl + '/v2/movie/top250';
         break;
     }
+    // 存储请求链接
+    this.data.requestUrl = dataUrl;
+    // 第一次的时候请求18条电影数据
+    dataUrl += '?count=18';
     util.getMovieList(dataUrl, this.handleData);
   },
   handleData: function (moviedata) {
+    this.data.isFreshing = true;
     var movies = [];
     var slogan = moviedata.title;
     for (var key in moviedata.subjects) {
@@ -59,15 +68,33 @@ Page({
       };
       movies.push(temp);
     }
-
+    // 如果原来就加载有电影数据，就将新获取到的数据合并到原来的数组中
+    if (!this.data.isEmpty) {
+      var totalMovies = this.data.movies.concat(movies);
+    } else {
+      // 如果是第一次加载就直接赋值给totalMovies，同时改变标记
+      totalMovies = movies;
+      this.data.isEmpty = false;
+    }
     this.setData({
-      movies: movies
+      movies: totalMovies
     });
+    // 每次加载，加载总数就加18
+    this.data.totalCount += 18;
   },
-  onReady:function(){
+  onReady: function () {
     wx.setNavigationBarTitle({
       title: this.data.navigationTitle,
     })
+  },
+  onScrollLower: function () {
+    if (!this.data.isFreshing) {
+      console.log(this.data.isFreshing)
+      var nextUrl = this.data.requestUrl + '?start=' + this.data.totalCount + '&count=18';
+      console.log(nextUrl)
+      util.getMovieList(nextUrl, this.handleData);
+      this.data.isFreshing = false;
+      console.log(this.data.isFreshing)
+    }
   }
-
 })

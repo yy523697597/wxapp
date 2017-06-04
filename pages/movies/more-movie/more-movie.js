da6ef1a93ca25b0c52c1ba37a68f84393d0a5451
yu yi <523697597@qq.com>
2017-06-04 10:54:16 +0800

    V4.4 动态设置更多电影页面的导航栏标题

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    category: ''
   },
 
   /**
@@ -13,55 +13,60 @@ Page({
    */
   onLoad: function (options) {
     var category = options.category;
-    console.log(category);
+    this.setData({
+      category: category
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
+    wx.setNavigationBarTitle({
+      title: this.data.category,
+    })
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
